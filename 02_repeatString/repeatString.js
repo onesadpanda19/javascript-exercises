fc51b979d34385a308da47657090e2dcc70d510f
Andrew Nelson <nelsand19@gmail.com>
2023-06-01 19:02:14 -0600

    complete corses 1 through 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    if (times <= -1) {
+        return "ERROR";
+    };
+    let newString = "";
+    for (let i = 0; i < times; i++){
+      newString += string;
+    };
+    return newString;
 
-};
+}
 
 // Do not edit below this line
 module.exports = repeatString;
