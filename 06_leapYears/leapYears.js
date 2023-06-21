26026882a92340f8c91dbbd10f5a6246d1dcd0eb
Andrew Nelson <nelsand19@gmail.com>
2023-06-20 18:53:43 -0600

    completed exercises 6 and 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,7 @@
-const leapYears = function() {
-
-};
+const leapYears = function(years) {
+if (!(years % 4) && years % 100 || !(years % 400) ) { return true 
+} else {return false}
+}
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+let newNum = (num - 32) * (5/9) ;
+  console.log (newNum)
+ let newNum2 = Math.round (newNum * 10) / 10
+  return newNum2
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+ let newNum = (num * (9/5) + 32);
+ console.log (newNum)
+ let newNum2 = Math.round (newNum * 10) / 10
+ return newNum2 
 };
 
 // Do not edit below this line
