5beb4882806b7ec2890c7a88a985ba44e668247a
Andrew Nelson <nelsand19@gmail.com>
2023-08-24 18:56:08 -0600

    did test calculator, palendromes, and fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(p) {
+    if (p < 0 )
+    return "OOPS"
+const n = p
+let n1 = 0, n2 = 1, next
+for (let i = 1; i <= p; i++) {
+   next = n1 + n2;
+    n1 = n2;
+    n2 = next;
+}
+return n1
 };
 
 // Do not edit below this line
